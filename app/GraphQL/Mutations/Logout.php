e5bba3a54c0129d1e890f3eefb893c96c1978c61
Renaud <renaudfradin@gmail.com>
2024-04-27 19:55:21 +0200

    add login and logout and secure all route and refracto

---
+++ app/GraphQL/Mutations/Logout.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use Illuminate\Support\Facades\Auth;
+
+final readonly class Logout
+{
+    public function __invoke()
+    {
+        Auth::user()->currentAccessToken()->delete();
+
+        return "Successfully logged out";
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,12 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasFactory;
+    use HasApiTokens;
     use Notifiable;
 
     protected $guarded = ['id'];
